50d98524da093c037e05f75b00b2f466f6085a6e
meghabyte <meghas@stanford.edu>
2017-07-17 13:41:30 -0700

    created generate utterance file

---
+++ plot/generate_utterance.java
@@ -0,0 +1,28 @@
+import java.util.*
+
+public class CanonicalUtterance
+{
+	String path;
+	String value;
+	public CanonicalUtterance() { }
+	public CanonicalUtterance(String path, String value) {
+		this.path = path;
+		this.value = value;
+	}
+
+
+	public String simple_canonical_utterance()
+	{
+
+	}
+
+
+	public String complex_canonical_utterance()
+	{
+
+	}
+
+}
+
+
+
